5b7e5d9737a3ffac7d016e81e1c5479404b0c770
Ezabulbary <ezabul.bary@gmail.com>
2022-06-03 21:37:41 +0600

    install stripe payment method

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const jwt = require('jsonwebtoken');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
@@ -146,7 +146,7 @@ async function run() {
 
         app.get('/booking/:id', verifyJWT, async (req, res) => {
             const id = req.params.id;
-            const query = { _id: ObjectId(id) };
+            const query = {_id: ObjectId(id)};
             const booking = await bookingCollection.findOne(query);
             res.send(booking);
         })
