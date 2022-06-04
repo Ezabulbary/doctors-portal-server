6a5b49892f261af883511b0b1d5cb43f8eff32dc
Ezabulbary <ezabul.bary@gmail.com>
2022-06-04 23:41:52 +0600

    create payment intent

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 app.use(cors());
 app.use(express.json());
@@ -74,7 +75,7 @@ async function run() {
             };
             const result = await userCollection.updateOne(filter, updateDoc);
             res.send(result);
-        })
+        });
 
         app.put('/user/:email', async (req, res) => {
             const email = req.params.email;
@@ -87,7 +88,20 @@ async function run() {
             const result = await userCollection.updateOne(filter, updateDoc, options);
             const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1d' })
             res.send({ result, token });
-        })
+        });
+
+        app.post("/create-payment-intent", verifyJWT, async (req, res) => {
+            const service = req.body;
+            const price = service.price;
+            const amount = price*100;
+            const paymentIntent = await stripe.paymentIntents.create({
+                amount: amount,
+                currency: "usd",
+                payment_method_types:['card']
+            });
+
+            res.send({clientSecret: paymentIntent.client_secret});
+        });
 
 
         // Warning: This is not the proper way to query multiple collection.
